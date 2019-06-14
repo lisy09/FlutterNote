b291cb143f58e2c88336fdc9afd4a3721a29e701
suiyan-li <suiyan.li@dena.com>
2019-06-14 10:50:06 +0900

    add tutorial

--- GSYFlutterBook/github_client/lib/widget/tabbar_widget.dart
+++ GSYFlutterBook/github_client/lib/widget/tabbar_widget.dart
@@ -12,43 +12,66 @@ class TarBarWidgetControl {
 class TabBarWidget extends StatefulWidget {
   final TarBarType type;
   final List<Widget> tabItems;
+  final List<Widget> tabViews;
+  final Color indicatorColor;
   final bool resizeToAvoidBottomPadding;
+  final Widget title;
+  final Widget drawer;
   final Widget floatingActionButton;
   final FloatingActionButtonLocation floatingActionButtonLocation;
   final TarBarWidgetControl tarbarWidgetControl;
+  final ValueChanged<int> onPageChaned;
 
   TabBarWidget({
     Key key,
     this.type,
     this.tabItems,
+    this.tabViews,
+    this.indicatorColor,
+    this.title,
+    this.drawer,
     this.floatingActionButton,
     this.floatingActionButtonLocation,
     this.resizeToAvoidBottomPadding = true,
     this.tarbarWidgetControl,
+    this.onPageChaned,
   }) : super(key:key)
 
   @override
   _TabBarWidgetState createState() => _TabBarWidgetState(
     this.type,
-    this.tabItems,
+    this.tabViews,
+    this.indicatorColor,
+    this.title,
+    this.drawer,
     this.floatingActionButton,
     this.tarbarWidgetControl,
+    this.onPageChaned,
   );
 }
 
 class _TabBarWidgetState extends State<TabBarWidget> 
 with SingleTickerProviderStateMixin {
   final TarBarType _type;
-  final List<Widget> _tabView;
-  // final Color _indicatorColor;
+  final List<Widget> _tabViews;
+  final Color _indicatorColor;
+  final Widget _title;
+  final Widget _drawer;
   final Widget _floatingActionButton;
   final TarBarWidgetControl _tarbarWidgetControl;
 
+  final PageController _pageController = PageController();
+  final ValueChanged<int> _onPageChaned;
+
   _TabBarWidgetState(
     this._type,
-    this._tabView,
+    this._tabViews,
+    this._indicatorColor,
+    this._title,
+    this._drawer,
     this._floatingActionButton,
     this._tarbarWidgetControl,
+    this._onPageChaned,
   ) : super();
 
   TabController _tabController;
@@ -71,21 +94,46 @@ with SingleTickerProviderStateMixin {
   
   @override
   Widget build(BuildContext context) {
-    if (this._type == TarBarType.Top) {
-      return new Scaffold(
-        resizeToAvoidBottomPadding: widget.resizeToAvoidBottomPadding,
-        floatingActionButton: SafeArea(child: _floatingActionButton ?? Container()),
-        floatingActionButtonLocation: widget.floatingActionButtonLocation,
-        persistentFooterButtons: _tarbarWidgetControl == null ? 
-          null : _tarbarWidgetControl.footerButton,
-        appBar: new AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          title: ,
+    // if (this._type == TarBarType.Top) {
+    //   return new Scaffold(
+
+    //     resizeToAvoidBottomPadding: widget.resizeToAvoidBottomPadding,
+    //     floatingActionButton: SafeArea(child: _floatingActionButton ?? Container()),
+    //     floatingActionButtonLocation: widget.floatingActionButtonLocation,
+    //     persistentFooterButtons: _tarbarWidgetControl == null ? 
+    //       null : _tarbarWidgetControl.footerButton,
+    //     appBar: new AppBar(
+    //       backgroundColor: Theme.of(context).primaryColor,
+    //       title: ,
+    //     ),
+    //   );
+    // }
+    
+    // bottom tab
+    return new Scaffold(
+      // set drawer for side swipe
+      drawer: _drawer,
+      appBar: new AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: _title,
+      ),
+      // PageView for tabs
+      body: new PageView(
+        // controller synced with tabBar's controller
+        controller: _pageController,
+        children: _tabViews,
+        onPageChanged: (index) {
+          _tabController.animateTo(index);
+        },
+      ),
+      bottomNavigationBar: new Material(
+        color: Theme.of(context).primaryColor,
+        child: new TabBar(
+          controller: _tabController,
+          tabs: widget.tabItems,
+          indicatorColor: _indicatorColor,
         ),
-      );
-    }
-    return Container(
-      
+      ),
     );
   }
 }
--- Tutorial.md
+++ Tutorial.md
@@ -1,6 +1,7 @@
 # books
 - https://github.com/CarGuo/GSYFlutterBook
 - https://flutterchina.club/tutorials/
+- [flutter实战](https://book.flutterchina.club/)
 
 # source code
 https://github.com/CarGuo/GSYGithubAppFlutter
